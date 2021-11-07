e86405fc0704f23b233ec4d7c7332229109a63bf
Samet Koyuncu <sametkoyuncu@live.com>
2021-11-07 21:10:45 +0300

    End of 'Chakra UI Kurulumu'

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Signin from './pages/Auth/Signin'
+import Signup from './pages/Auth/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Switch>
+            <Route path="/" exact component={} />
+            <Route path="/about" component={Signin} />
+            <Route path="/about" component={Signup} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
