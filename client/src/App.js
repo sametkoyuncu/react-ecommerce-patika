84df83edb360d498504d64b786482046260cfe7f
Samet Koyuncu <sametkoyuncu@live.com>
2021-11-08 15:31:05 +0300

    Add chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Signin from './pages/Auth/Signin'
+import Signup from './pages/Auth/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Routes>
+            {/* <Route path="/" exact component={} /> */}
+            <Route path="/about" component={Signin} />
+            <Route path="/about" component={Signup} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
+import { Button } from '@chakra-ui/react'
 
 function Navbar() {
   return (
@@ -9,7 +10,7 @@ function Navbar() {
         <div className="logo">
           <Link to="/">eCommerce</Link>
         </div>
-        <ul className={styles.mwnu}>
+        <ul className={styles.menu}>
           <li>
             <Link to="/">Products</Link>
           </li>
